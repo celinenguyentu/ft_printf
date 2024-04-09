19480a8c842f9404dfa0f2e87a6bcdf8e30f9b74
celinenguyentu <celine.nguyentu@gmail.com>
2024-04-09 17:57:55 +0200

    Add header file

---
+++ ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnguyen- <cnguyen-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 23:46:33 by cnguyen-          #+#    #+#             */
+/*   Updated: 2024/04/09 17:57:15 by cnguyen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+// Libft
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+// ft_printf
+int		ft_printf(const char *format, ...);
+
+#endif
